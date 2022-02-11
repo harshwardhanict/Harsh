abfedbaf7fb1376f2f41ce403cfa422499db409a
harsh <harshwardhan@icreativetechnolabs.com>
2022-02-11 18:37:20 +0530

    [ADD]function.py

---
+++ function.py
@@ -0,0 +1,50 @@
+# def duplication_sum(a,b):
+#     if a+b>1000:print(a*b)
+#     else:print(a+b)
+#
+# duplication_sum(20,30)
+from recursion import l
+
+#
+# def previous():
+#     l,sum=[],0
+#     for x in range(10):
+#         l.append(x)
+#         sum +=x
+#     for i in range(len(l)):
+#      for j in l:print("the previous" ,l[i],"present no is",l[i+1] )#"sum :",sum[0])
+#
+# previous()
+
+
+# def student(n):
+#     class1=[12,3,4,5,56,45,1,2]
+#     if n in class1:print(True)
+#     else:print(False)
+#
+# student(12)
+#
+# def odd_even(n):
+#     if n%2==0:print("even")
+#     else:print("odd")
+#
+# odd_EVEN(23)
+
+# def v_c():
+#     n,count,count1=input("hh :"),0,0
+#     vowels=["a","u","e","i","o","A","U","E","I","O"]
+#     for x in n:
+#         if x in vowels:
+#             count+=1
+#         else:count1+=1
+#     print("This Vowels :",count)
+#     print("This consonant :", count1)
+
+# def  g(x):
+#    if x == 1:
+#        return 1
+#    else:
+#        return x * g(x-1)
+#
+# print(g(5))
+
